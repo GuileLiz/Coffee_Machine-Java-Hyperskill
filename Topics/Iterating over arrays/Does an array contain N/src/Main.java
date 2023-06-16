add178dc60d30b5ad804d9eb3479bfa6eb1264e3
LizGuile <lizguile19@gmail.com>
2023-06-16 20:51:18 +0800

    Practice Iterating Does an array contain N?

--- Topics/Iterating over arrays/Does an array contain N/src/Main.java
+++ Topics/Iterating over arrays/Does an array contain N/src/Main.java
@@ -4,18 +4,27 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int lengthOfArray = scanner.nextInt();
+        int[] array = createArray(scanner, lengthOfArray);
+        System.out.print(isEqual(scanner, array));
+    }
+
+    public static int[] createArray(Scanner scanner, int lengthOfArray) {
         int[] array = new int[lengthOfArray];
         for (int iteration = 0; iteration < lengthOfArray; iteration++) {
             array[iteration] = scanner.nextInt();
         }
-        int isEqual = scanner.nextInt();
-        for (int initial = 0;  initial < lengthOfArray; initial++) {
-            if (array[initial] == isEqual) {
-                System.out.println(true);
-                break;
-            } else if (initial == lengthOfArray - 1 && array[initial] != isEqual) {
-                System.out.println(false);
+        return array;
+    }
+
+    public static boolean isEqual(Scanner scanner, int[] array) {
+        int inputVal = scanner.nextInt();
+        boolean result = false;
+        for (int number : array) {
+            if (number == inputVal) {
+                result = true;
             }
         }
+        return result;
     }
 }
+
