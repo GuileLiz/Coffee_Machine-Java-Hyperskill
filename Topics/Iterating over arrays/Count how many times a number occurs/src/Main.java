15516cab0bfaf9b4f9f92712ed6de7bedc1c8cd8
LizGuile <lizguile19@gmail.com>
2023-06-28 00:09:41 +0800

    Iterating over arrays practice

--- Topics/Iterating over arrays/Count how many times a number occurs/src/Main.java
+++ Topics/Iterating over arrays/Count how many times a number occurs/src/Main.java
@@ -7,26 +7,25 @@ class Main {
         int arraySize = scanner.nextInt();
         int[] array = createArray(arraySize, scanner);
         int valueToCompare = scanner.nextInt();
-        numberOfOccurence(valueToCompare, array);
+        int result = numberOfOccurence(valueToCompare, array);
+        System.out.print(array);
     }
 
     public static int[] createArray(int arraySize, Scanner scanner) {
         int[] array = new int[arraySize];
-        int tempMax = 0;
         for (int index = 0; index < array.length; index++) {
             array[index] = scanner.nextInt();
         }
         return array;
     }
 
-    public static void numberOfOccurence(int valueToCompare, int[] array) {
+    public static int numberOfOccurence(int valueToCompare, int[] array) {
         int maxValue = 0;
-        for (int element : array) {
-            if (element > maxValue) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] > maxValue) {
                maxValue += 1;
-               System.out.print(maxValue);
             }
         }
-        System.out.print(maxValue);
+        return maxValue;
     }
 }
